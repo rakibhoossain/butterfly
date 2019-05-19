86aeee7f10b46b34d2c7bc98def4ae6cc5338a1c
Rakib Hossain <rakibhoossain@gmail.com>
2019-05-19 23:56:28 +0600

    summary

--- front-page.php
+++ front-page.php
@@ -171,7 +171,7 @@ get_header();
                     the_post();
                       echo '<div class="col-md-4 col-sm-4 col-xs-12">';
 
-                      get_template_part( 'template-parts/loops/post', 'card' );
+                      get_template_part( 'template-parts/loops/post', 'summary' );
 
                     echo '</div>';
                   endwhile;
@@ -229,6 +229,26 @@ get_header();
       	<div class="container">
       		<div class="news_type_card">
             <div class="row">
+              
+
+
+
+                <?php
+                  /* Start the Loop */
+                  while ( have_posts() ) :
+                    the_post();
+                      echo '<div class="col-md-4 col-sm-4 col-xs-12">';
+
+                      get_template_part( 'template-parts/loops/post', 'card' );
+
+                    echo '</div>';
+                  endwhile;
+                ?>
+
+
+
+
+
               <div class="col-md-4 col-sm-4 col-xs-12">
                 <div class="card" style="width: ;">
                   <div class="card_header">
@@ -247,6 +267,7 @@ get_header();
                   </div>
                 </div>
               </div>
+
               <div class="col-md-4 col-sm-4 col-xs-12">
                 <div class="card" style="width: ;">
                   <div class="card_header">
@@ -265,24 +286,9 @@ get_header();
                   </div>
                 </div>
               </div>
-              <div class="col-md-4 col-sm-4 col-xs-12">
-                <div class="card" style="width: ;">
-                  <div class="card_header">
-                    <h3 style="background-color: #F8921A;"><span>সন্দেশ</span></h3>
-                  </div>
-                  <div class="news_type_overlay">
-	                <img class="card-img-top img-responsive" src="https://via.placeholder.com/320x200" alt="News Image">
-	                 <div class="card-body">
-	                   <h4 class="card-title" style="color: #0BB14D">আমার সোনার বাংলা</h4>
-	                    <p class="card-text">আমার সোনার বাংলা, আমি তোমায় ভালোবাসি..</p>
-	                 </div>
-	                 <a href="#" class="news_type_overlay_link"></a>
-                  </div>
-                  <div class="see_more">
-                    <a href="#" class="" style="border-top: 2px solid #F8921A;">আরও</a>
-                  </div>
-                </div>
-              </div>
+
+
+
             </div>
           </div>
       	</div>

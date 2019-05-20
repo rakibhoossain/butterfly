40ebbbf185f611b50d94195bee2648046427e6f1
Rakib Hossain <rakibhoossain@gmail.com>
2019-05-20 20:13:20 +0600

    footercredit

--- footer.php
+++ footer.php
@@ -10,21 +10,29 @@
  */
 
 ?>
-
       <footer id="footer">
-        <div class="container">
-          <div class="row">
-            
-          <?php
-            for($i=1;$i<=3;$i++) {
-              if (is_active_sidebar("footer-".($i))) :
-                dynamic_sidebar("footer-".($i));
-              endif;
-            }
-          ?>
+          <div class="container">
+            <div class="row">
+              
+            <?php
+              for($i=1;$i<=3;$i++) {
+                if (is_active_sidebar("footer-".($i))) :
+                  dynamic_sidebar("footer-".($i));
+                endif;
+              }
+            ?>
 
+            </div>
           </div>
-        </div>
+
+          <p class="footer-text col-md-12 mt-5 text-center">
+      <?php
+        /* translators: 1: Theme name, 2: Theme author. */
+        $theme_author = 'Rakib Hossain' ;
+        $theme_author_url = 'https://facebook.com/prof.rakib';
+        printf( esc_html__( 'Theme: %1$s by %2$s', 'butterfly' ), 'butterfly', '<a href="'. esc_url($theme_author_url).'">'.esc_html($theme_author).'</a>' );
+      ?>
+        </p>
       </footer>
 
   	</div> <!-- Wrapper end  -->

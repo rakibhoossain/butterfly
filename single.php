1af8ccf048ae2647c3dd4a6140fc38a9c9ab77cd
Rakib Hossain <rakibhoossain@gmail.com>
2019-05-24 23:27:45 +0600

    Update single.php

--- single.php
+++ single.php
@@ -23,11 +23,7 @@ get_header();
 							while ( have_posts() ) : the_post();
 								get_template_part( 'template-parts/content', get_post_type() );
 
-								the_posts_pagination( array(
-									'prev_text' => '<i class="fa fa-arrow-left"></i><span class="screen-reader-text">' . __( 'Previous Page', 'butterfly' ) . '</span>',
-									'next_text' => '<span class="screen-reader-text">' . __( 'Next Page', 'butterfly' ) . '</span><i class="fa fa-arrow-right"></i>' ,
-									'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'butterfly' ) . ' </span>',
-								) );
+								the_post_navigation();
 
 								// If comments are open or we have at least one comment, load up the comment template.
 								if ( comments_open() || get_comments_number() ) :

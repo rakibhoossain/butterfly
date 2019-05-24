4772e76b4128dc9a28114d7f117608d3ca22215c
Rakib Hossain <rakibhoossain@gmail.com>
2019-05-25 01:03:57 +0600

    page

--- page.php
+++ page.php
@@ -14,27 +14,36 @@
 
 get_header();
 ?>
+<div class="container breadcrumb-container">
+	<?php magazil_breadcrumbs(); ?>
+</div>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<section id="single_news">
+        		<div class="container">
+        			<div class="row">
+        				<div class="col-md-9">
+							<?php
+							while ( have_posts() ) : the_post();
+								get_template_part( 'template-parts/content', 'page' );
+								the_post_navigation();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
+									comments_template();
+								endif;
+							endwhile; // End of the loop.
+							?>
+        				</div>
+        				<div class="col-md-3">
+        					<?php get_sidebar();?>
+        				</div>
+        			</div>
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+        		</div>
+      		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
-get_sidebar();
 get_footer();
